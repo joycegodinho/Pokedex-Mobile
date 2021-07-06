8c5d98f1f93cf8ad26fe0b8608852003edab9383
Joyce Bosco <joycegodinhobosco@gmail.com>
2021-07-06 17:15:29 -0300

    first layout of stats in details implemented

--- src/screens/details.js
+++ src/screens/details.js
@@ -3,6 +3,7 @@ import {View, Text, ScrollView, Image, TouchableOpacity, StyleSheet, TextInput, 
 import styled from 'styled-components/native';
 
 
+
 const ViewDetails = styled.View`
     flex: 1;
     align-items: center;
@@ -20,6 +21,24 @@ const StyledIndicator = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
+`
+const StyledFill = styled.View`
+    background-color: #a8d9d5;
+    border-radius: 5px;
+   
+    height: 5px;
+
+`
+
+const StyledUnFill = styled.View`
+    background-color: #7b4173;
+    border-radius: 5px;
+    
+    height: 5px;
+`
+
+const StyledStats = styled.View`
+    margin: 10px;
 `
 
 const Details = props => {
@@ -40,6 +59,11 @@ const Details = props => {
       console.log(details)
 
 
+
+
+      const colors = [ '#7b4173', '#a55194']
+      const keys   = [ 'positive', 'negative']
+
     return details.name ? (
         <ViewDetails>
             <StyledImage 
@@ -53,8 +77,56 @@ const Details = props => {
             <StyledText>Name: {details.name}</StyledText>
             <StyledText>Height: {details.height}</StyledText>
             <StyledText>Weight: {details.weight}</StyledText>
-            <StyledText>Ability: {details.abilities[0].ability.name}</StyledText>
-            <StyledText>Type: {details.types[0].type.name}</StyledText>
+            {details.abilities[1] ? (
+                <View>
+                    <StyledText>Ability: {details.abilities[0].ability.name}</StyledText>
+                    <StyledText>Ability 2: {details.abilities[1].ability.name}</StyledText>
+                </View>
+            ):(
+                <StyledText>Ability: {details.abilities[0].ability.name}</StyledText>
+            )}
+            {details.types[1] ? (
+                <View>
+                    <StyledText>Type: {details.types[0].type.name}</StyledText>
+                    <StyledText>Type 2: {details.types[1].type.name}</StyledText>
+                </View>
+            ):(
+                <StyledText>Type: {details.types[0].type.name}</StyledText>
+            )}
+
+            <StyledStats>
+                <Text><StyledFill style={{width:details.stats[0].base_stat}}></StyledFill><StyledUnFill style={{width: 300 - details.stats[0].base_stat}}></StyledUnFill></Text>
+            </StyledStats>
+            <StyledStats>
+                <Text><StyledFill style={{width:details.stats[1].base_stat}}></StyledFill><StyledUnFill style={{width: 300 - details.stats[1].base_stat}}></StyledUnFill></Text>
+            </StyledStats>
+            <StyledStats>
+                <Text><StyledFill style={{width:details.stats[2].base_stat}}></StyledFill><StyledUnFill style={{width: 300 - details.stats[2].base_stat}}></StyledUnFill></Text>
+            </StyledStats>
+            <StyledStats>
+                <Text><StyledFill style={{width:details.stats[3].base_stat}}></StyledFill><StyledUnFill style={{width: 300 - details.stats[3].base_stat}}></StyledUnFill></Text>
+            </StyledStats>
+            <StyledStats>
+                <Text><StyledFill style={{width:details.stats[4].base_stat}}></StyledFill><StyledUnFill style={{width: 300 - details.stats[4].base_stat}}></StyledUnFill></Text>
+            </StyledStats>
+            <StyledStats>
+                <Text><StyledFill style={{width:details.stats[5].base_stat}}></StyledFill><StyledUnFill style={{width: 300 - details.stats[5].base_stat}}></StyledUnFill></Text>
+            </StyledStats>
+
+            
+            
+            
+
+
+            <Text>Stats</Text>
+            <StyledText>HP: {details.stats[0].base_stat}</StyledText>
+            <StyledText>Attack: {details.stats[1].base_stat}</StyledText>
+            <StyledText>Defense: {details.stats[2].base_stat}</StyledText>
+            <StyledText>Special-Attack: {details.stats[3].base_stat}</StyledText>
+            <StyledText>Special-Defense: {details.stats[4].base_stat}</StyledText>
+            <StyledText>Speed: {details.stats[5].base_stat}</StyledText>
+
+
             
         </ViewDetails>
 
